3e00e11611bf59810346396038829a0cb41cc01c
Daniele Torino <Daniele.Torino@fides.ch>
2018-07-06 16:00:51 +0200

    * cleanup

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,24 @@
-module.exports = {
-    plugins: [
-        require('postcss-replace')({
+/* eslint-disable */
+/* jshint node: true */
+'use strict';
+
+module.exports = (ctx) => ({
+    map: ctx.file.dirname.includes('examples') ? false : {
+        inline: false,
+        annotation: true,
+        sourcesContent: true
+    },
+    plugins: {
+        autoprefixer: {
+            cascade: false
+        },
+        "postcss-replace": {
             pattern: '(~fides-helvetica\/fonts)',
             data: {
                 '~fides-helvetica/fonts': '..\/fonts'
             }
-        })
-    ]
-}
+        }
+    }
+});
+
+
